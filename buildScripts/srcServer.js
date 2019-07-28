62e5ea52dc40158a1a55a313afdc35d08604d13a
winni4eva <adams4lyf@gmail.com>
2019-07-28 07:28:17 +0000

    en(biuld): added localtunnel script

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -7,4 +7,12 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../src/index.html'));
-})
+});
+
+app.listen(port, err => {
+    if (err) {
+        console.log(err);
+    } else {
+        open('http://localhost:' + port);
+    }
+});

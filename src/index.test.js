699825cdd6ff3e48392634abdd445e4850e36d1d
winni4eva <adams4lyf@gmail.com>
2019-08-06 00:18:11 +0000

    en(mock): added mock users data

--- src/index.test.js
+++ src/index.test.js
@@ -9,11 +9,11 @@ describe("Our first test", () => {
 });
 
 describe("index.html", () => {
-    it('it should say hello', (done) => {
+    it('it should have h3  that says Users', (done) => {
         const index = fs.readFileSync('./src/index.html', 'utf-8');
         jsdom.env(index, (err, window) => {
             const h3 = window.document.getElementsByTagName('h3')[0];
-            expect(h3.innerHTML).to.equal("Hello World!");
+            expect(h3.innerHTML).to.equal("Users");
             done();
             window.close();
         });
